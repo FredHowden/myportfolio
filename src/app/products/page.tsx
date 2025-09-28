771ea3c5e14fc43847f162e7f16c90f32904c051
FredHowden <fredhhowden@gmail.com>
2025-09-28 21:27:51 +0200

    added basic dynamic routing example

--- src/app/products/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function ProductList() {
-  return (
-    <ul>
-      <li><a href="/products/1">Product 1</a></li>
-      <li><a href="/products/2">Product 2</a></li>
-      <li><a href="/products/3">Product 3</a></li>
-    </ul>
-  );
-}
--- src/components/sections/projects.tsx
+++ src/components/sections/projects.tsx
@@ -13,16 +13,18 @@ export default async function ProjectsSection() {
 
                 {projects.map((project) => (
                     <li key={project.name} className="rounded-md shadow-md max-sm:p-2 max-sm:w-[100%] max-md:w-[80%] md:w-[45%] sm:p-3">
-                        <figure className="">
-                            <div className="relative aspect-[9/11] xs:aspect-video">
-                                <Image className="hidden xs:block object-contain" fill alt="" src={project.images.medium_screen} />
-                                <Image className="xs:hidden object-contain" alt="" fill src={project.images.small_screen} />
-                            </div>
-                            <figcaption className="pt-3">
-                                <h3 className="text-2xl">{project.name}</h3>
-                                <p>{project.description}</p>
-                            </figcaption>
-                        </figure>
+                        <a href={project.id}>
+                            <figure className="">
+                                <div className="relative aspect-[9/11] xs:aspect-video">
+                                    <Image className="hidden xs:block object-contain" fill alt="" src={project.images.medium_screen} />
+                                    <Image className="xs:hidden object-contain" alt="" fill src={project.images.small_screen} />
+                                </div>
+                                <figcaption className="pt-3">
+                                    <h3 className="text-2xl">{project.name}</h3>
+                                    <p>{project.description}</p>
+                                </figcaption>
+                            </figure>
+                        </a>
                     </li>
                 ))}
 
